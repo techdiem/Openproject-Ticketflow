3d4709de7c18b39e17dff7181ede59906447b2bc
techdiem <48885218+techdiem@users.noreply.github.com>
2024-08-13 18:03:08 +0200

    Switch to disable comment2mail

--- ticketflow.py
+++ ticketflow.py
@@ -4,12 +4,14 @@
 
 from processes.mailprocess import MailProcess
 from processes.notificationprocess import NotificationProcess
+from config import config
 
 def main():
-    mailProcess = MailProcess()
-    mailProcess.run()
-    notificationProcess = NotificationProcess()
-    notificationProcess.run()
+    # mailProcess = MailProcess()
+    # mailProcess.run()
+    if config.get('Workflow', 'comment_to_mail') == "true":
+        notificationProcess = NotificationProcess()
+        notificationProcess.run()
 
 if __name__ == "__main__":
     main()
