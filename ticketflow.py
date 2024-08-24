0f2c9344c1f63c2782f5faa11eed9051f0b77192
techdiem <48885218+techdiem@users.noreply.github.com>
2024-08-24 17:11:18 +0200

    Replace InsecureRequestWarning when verification is disabled by custom log

--- ticketflow.py
+++ ticketflow.py
@@ -5,8 +5,13 @@
 from processes.mailprocess import MailProcess
 from processes.notificationprocess import NotificationProcess
 from config import config
+from logger import logger
+import urllib3
 
 def main():
+    if config.get("OpenProject", "https_verification") == "false":
+        logger.warning("Critical: HTTPS Zertifikats-Verifikation ist deaktiviert! -> Unsichere Verbindung zur OpenProject-API!")
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
     mailProcess = MailProcess()
     mailProcess.run()
     if config.get('Workflow', 'comment_to_mail') == "true":
