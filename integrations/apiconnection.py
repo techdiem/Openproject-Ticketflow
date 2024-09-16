0a3ead78dcd5e508a3c6b79215bb10dca38f837b
techdiem <48885218+techdiem@users.noreply.github.com>
2024-09-16 18:52:44 +0200

    Get correct type for https validation from config

--- integrations/apiconnection.py
+++ integrations/apiconnection.py
@@ -5,7 +5,7 @@ def post_request(context, data=None, files=None, headers=None):
     r = requests.post(f'{config.get("OpenProject", "base_url")}{context}',
                         auth=('apikey', config.get("OpenProject", "api_key")),
                         data=data,
-                        verify=config.get("OpenProject", "https_verification") == "true",
+                        verify=config.getboolean("OpenProject", "https_verification"),
                         headers=headers,
                         files=files,
                         timeout=30)
@@ -14,7 +14,7 @@ def post_request(context, data=None, files=None, headers=None):
 def get_request(context, headers=None, params=None):
     r = requests.get(f'{config.get("OpenProject", "base_url")}{context}',
                      auth=('apikey', config.get("OpenProject", "api_key")),
-                     verify=config.get("OpenProject", "https_verification") == "true",
+                     verify=config.getboolean("OpenProject", "https_verification"),
                      headers=headers,
                      params=params,
                      timeout=30)
@@ -23,7 +23,7 @@ def get_request(context, headers=None, params=None):
 def patch_request(context, headers=None, data=None):
     r = requests.patch(f'{config.get("OpenProject", "base_url")}{context}',
                      auth=('apikey', config.get("OpenProject", "api_key")),
-                     verify=config.get("OpenProject", "https_verification") == "true",
+                     verify=config.getboolean("OpenProject", "https_verification"),
                      headers=headers,
                      data=data,
                      timeout=30)
